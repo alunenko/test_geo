a24d9b07c28a9995df61e271a32ae23daa496b6d
Anton <antoni.lunenko@gmail.com>
2019-05-13 02:05:39 +0300

    feature/design: based on functionality, create theme, animation

--- test-issue/src/app/components/autocomplete/autocomplete.component.ts
+++ test-issue/src/app/components/autocomplete/autocomplete.component.ts
@@ -1,24 +1,48 @@
 import {
-    Component,
+    Component, ElementRef, Renderer2,
     ViewEncapsulation
 } from '@angular/core';
 
 import {GeoService} from "../../services/geo.service";
 import {AutocompleteResultService} from "../autocomplete-result/autocomplete-result.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
     selector: 'app-autocomplete',
     templateUrl: './autocomplete.component.html',
     styleUrls: ['./autocomplete.component.scss'],
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    animations: [
+        trigger('toggleSearchBar', [
+            state('open', style({
+                animation: 'containerBounce .4s',
+                width: 'calc(100% + 50px)',
+                right: '-50px'
+            })),
+            state('closed', style({
+                animation: 'containerBounce .4s',
+                width: '60px',
+                right: '-5px'
+            })),
+            transition('open => closed', [
+                animate('.4s')
+            ]),
+            transition('closed => open', [
+                animate('.4s')
+            ]),
+        ])
+    ]
 })
 export class AutocompleteComponent {
-
     autocompliteValue = "";
+    isSearchBarOpen = false;
+    isOpen = false;
 
     constructor(
         private geoService: GeoService,
-        private autocompliteResultService: AutocompleteResultService
+        private autocompliteResultService: AutocompleteResultService,
+        private elementReference: ElementRef,
+        private renderer2: Renderer2
     ) {
         this.geoService.newSelected.subscribe(
             (newValue) => {
@@ -32,6 +56,40 @@ export class AutocompleteComponent {
 
         if (isGeonamesResult) {
             this.autocompliteResultService.showDisplayResults();
+        }
+    }
+
+    onOpen() {
+        const className = "d-none";
+        const inputElement = this.elementReference.nativeElement.querySelector("input");
+        const isInput = inputElement.classList.contains(className);
+        const isCloseButtonElement = this.elementReference.nativeElement.querySelector("#close");
+
+        this.isOpen = true;
+        this.isSearchBarOpen = true;
+
+        if(isInput) {
+            this.renderer2.removeClass(inputElement, className);
+            inputElement.focus();
+            this.renderer2.removeClass(isCloseButtonElement, "hide");
+            this.renderer2.addClass(isCloseButtonElement, "show");
+        }
+    }
+
+    onClose() {
+        const className = "d-none";
+        const inputElement = this.elementReference.nativeElement.querySelector("input");
+        const isInput = inputElement.classList.contains(className);
+        const isCloseButtonElement = this.elementReference.nativeElement.querySelector("#close");
+
+        this.isOpen = false;
+        this.isSearchBarOpen = false;
+        this.autocompliteResultService.hideDisplayResults();
+
+        if(!isInput) {
+            this.renderer2.addClass(inputElement, className);
+            this.renderer2.addClass(isCloseButtonElement, "hide");
+            this.renderer2.removeClass(isCloseButtonElement, "show");
         }
     }
 }
